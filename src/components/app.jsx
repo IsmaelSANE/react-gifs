3ae10fd9666ce50df8ad51fbfaa9e511786c0316
Ismael Sane <six.echo.sa@gmail.com>
2019-09-23 19:30:02 +0200

    Implement the app component

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { component } from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="article">
+        <div className="left-scene"></div>
+        <div className="right-scene"></div>
+      </div>
+    );
+  }
+}
+export default App;
